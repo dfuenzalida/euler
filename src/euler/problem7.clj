3b4c826035572790444d7074901568bb2081b8b0
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-08-04 23:40:11 -0400

    solved problem #7

---
+++ src/euler/problem7.clj
@@ -0,0 +1,20 @@
+(ns euler.problem7)
+
+(defn euler-7 []
+
+  ;; test primality against a list of given primes
+  (defn prime? [primes num]
+    (loop [testing primes]
+          (if (empty? testing) true
+            (if (zero? (rem num (first testing))) false
+              (recur (rest testing))))))
+
+  (defn this-many-primes [num]
+    (loop [primes [2 3] current 3]
+      (if (= (count primes) num) primes
+          (if (prime? primes current)
+            (recur(conj primes current) (+ 2 current))
+            (recur primes (+ 2 current))))))
+
+  (last (this-many-primes 10001)))
+
