ad6ae6e823b26fb3f7a873b0c0022a1353de39e8
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-08-05 19:53:22 -0400

    solved #10, require Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject euler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solutions to problems from ProjectEuler.net"
+  :url "http://www.projectEuler.net"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ src/euler/problem10.clj
@@ -0,0 +1,23 @@
+(ns euler.problem10)
+
+;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;; Find the sum of all the primes below two million.
+
+(defn euler-10 []
+
+  ;; test primality against a list of given primes
+  (defn prime? [primes n]
+    (loop [pos 0]
+      (if (= pos (count primes)) true
+          (if (zero? (rem n (nth primes pos))) false
+              (recur (inc pos))))))
+
+  (defn primes-up-to [num]
+    (loop [primes [2 3] current 3]
+      (if (> current num) primes
+          (if (prime? primes current)
+            (recur (conj primes current) (+ 2 current))
+            (recur primes (+ 2 current))))))
+
+  (apply + (primes-up-to 2000000)))
+
