b543bfd40905b7b49324bfe0df144327086eeba1
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-09-09 18:28:07 -0300

    fixed typo

--- src/euler/problem20.clj
+++ src/euler/problem20.clj
@@ -4,11 +4,11 @@
 
   ;; Find the sum of the digits in the number 100!
 
-  ;; fibonacci
-  (defn fib[n] (reduce *' (range 1 (inc n))))
+  ;; factorial
+  (defn factorial[n] (reduce *' (range 1 (inc n))))
 
   (reduce
    +
    (map
     #(- (int %1) (int \0))
-    (str (fib 100)))))
+    (str (factorial 100)))))
