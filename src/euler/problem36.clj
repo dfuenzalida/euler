d51ef8456377266c1a8a95ba5b8b4cf9e79646d3
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-09-21 21:30:24 -0300

    solved problem #36

---
+++ src/euler/problem36.clj
@@ -0,0 +1,15 @@
+(ns euler.problem36)
+
+
+(defn euler-36 []
+
+  (defn palin? [x] (= (apply str (reverse x)) x))
+
+  (defn both-palin? [n]
+    (and
+     (palin? (str n))
+     (palin? (Integer/toBinaryString n))))
+
+  (apply + (filter both-palin? (range 1 1000000))))
+
+(println (euler-36))
