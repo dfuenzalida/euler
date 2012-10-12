6277f36105229a4ccf5ab04f08333fd0caac7f65
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2012-10-11 21:08:39 -0300

    solved problem #392

--- src/euler/problem39.clj
+++ src/euler/problem39.clj
@@ -2,10 +2,36 @@
 
 (defn euler-39 []
 
-  (def terms
-    (for [x (range 2 101) y (range 2 101)]
-      (.pow (BigInteger. (str x)) y)))
+  ;; If p is the perimeter of a right angle triangle with integral
+  ;; length sides, {a,b,c}, there are exactly three solutions for
+  ;; p = 120
+  ;;
+  ;; {20,48,52}, {24,45,51}, {30,40,50}
+  ;;
+  ;; For which value of p <= 1000, is the number of solutions maximised?
 
-  (count (apply conj #{} terms)))
+  (defn valid? [triplet]
+    (let [a (first (sort triplet))
+          b (second (sort triplet))
+          c (nth (sort triplet) 2)]
+      (= (* c c) (+ (* a a) (* b b)))))
+
+  (defn all-triangles [perimeter]
+    (for [a (range 1 (inc perimeter))
+          b (range 1 (inc perimeter))
+          :while (and
+                  (< b a)
+                  (< (+ a b) perimeter))]
+      [a b (- perimeter a b)]))
+
+  (loop [i 3 max-index 0 max-count 0]
+    (if (= 1001 i)
+      [max-index max-count]
+      (let [c (count (filter valid? (all-triangles i)))]
+        (if (> c max-count)
+          (recur (inc i) i c)
+          (recur (inc i) max-index max-count)))))
+
+  )
 
 (println (euler-39))
