349fd9b1010fd0cf06ad0ae1331a080101a5e180
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-07-13 18:35:50 -0400

    Simpler solution

--- src/euler/problem19.clj
+++ src/euler/problem19.clj
@@ -1,19 +1,17 @@
 (ns euler.problem19
-  (:import (java.util Calendar Locale TimeZone)
-           (java.text SimpleDateFormat)))
+  (:import (java.util Calendar)))
 
 ;; How many Sundays fell on the first of the month during the twentieth century
 ;; (1 Jan 1901 to 31 Dec 2000)?
 
 (defn euler-19 []
 
-  (def datefmt (SimpleDateFormat. "dd/MM/yyyy"))
-  (def weekday (SimpleDateFormat. "EEE" Locale/US))
+  (defn sunday? [d m y]
+    (let [cal (doto (Calendar/getInstance) (.set y (dec m) d))]
+      (= Calendar/SUNDAY (.get cal Calendar/DAY_OF_WEEK))))
 
   (count
-   (for [y (range 1901 2001) m (range 1 (inc 12))
-         :when (= "Sun" (.format weekday (.parse datefmt (format "01/%02d/%04d" m y))))]
-     [:anything])))
+   (for [y (range 1901 (inc 2000)) m (range 1 (inc 12)) :when (sunday? 1 m y)] :ok)))
 
 ;; $ lein repl
 ;; user=> (use 'euler.problem19)
