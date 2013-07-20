d5ad9e6915489c92d3bc44f186e78eb255b336f3
Denis Fuenzalida <denis.fuenzalida@gmail.com>
2013-07-20 14:02:25 -0400

    Reimplemented with for

--- src/euler/problem09.clj
+++ src/euler/problem09.clj
@@ -1,4 +1,4 @@
-(ns euler.problem9)
+(ns euler.problem09)
 
 ;; A Pythagorean triplet is a set of 3 natural numbers, a  b  c, for which,
 ;;   a^2 + b^2 = c^2
@@ -7,13 +7,9 @@
 ;;
 ;; There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 ;; Find the product a * b * c.
-;;
 
 (defn euler-9 []
-  (apply *
-         (loop [a 1 b 1]
-           ;; a + b + c = 1000 ==> c = 1000 - a - b
-           (let [c (- 1000 a b)]
-             (if (= a 999) (recur 1 (inc b))
-                 (if (= (+ (* a a) (* b b)) (* c c)) [a b c]
-                     (recur (inc a) b)))))))
+  (for [x (range 1 1000) y (range x 1000)
+        :when (= (+ (* x x) (* y y))
+                 (* (- 1000 x y) (- 1000 x y)))]
+    (* x y (- 1000 x y))))
